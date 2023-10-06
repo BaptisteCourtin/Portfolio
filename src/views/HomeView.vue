009f7f31dc8842dcc54ba547652c7f93dff960f4
Baptiste Courtin <bap2910@gmail.com>
2023-10-06 12:10:25 +0200

    changement recherche

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,57 +21,65 @@ import { faArrowTrendDown } from '@fortawesome/free-solid-svg-icons'
 import { faLocationArrow } from '@fortawesome/free-solid-svg-icons'
 import { faAnglesDown } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faArrowRight, faArrowLeftLong, faUpLong, faArrowTurnDown, faArrowRotateRight, faArrowTrendUp, faArrowTrendDown, faLocationArrow, faAnglesDown)
+library.add(
+  faArrowRight,
+  faArrowLeftLong,
+  faUpLong,
+  faArrowTurnDown,
+  faArrowRotateRight,
+  faArrowTrendUp,
+  faArrowTrendDown,
+  faLocationArrow,
+  faAnglesDown
+)
 </script>
 
 <template>
   <div class="global-home">
-    <h1>{{ $t("accueil.bonjour") }}</h1>
+    <h1>{{ $t('accueil.bonjour') }}</h1>
 
     <div class="moi">
       <div class="text">
         <p>
-          {{ $t("accueil.moi") }} <span class="name">Baptiste COURTIN</span>,
-          <span class="dev">{{ $t("accueil.metier") }}</span>
+          {{ $t('accueil.moi') }} <span class="name">Baptiste COURTIN</span>,
+          <span class="dev">{{ $t('accueil.metier') }}</span>
         </p>
-        <p>{{ $t("accueil.photo") }} <font-awesome-icon icon="fa-solid fa-arrow-right" /></p>
+        <p>{{ $t('accueil.photo') }} <font-awesome-icon icon="fa-solid fa-arrow-right" /></p>
       </div>
 
       <div class="box-tourne">
-        <img src="../assets/moi/photoProfil1.jpg" alt="moi en photo" loading="lazy"/>
+        <img src="../assets/moi/photoProfil1.jpg" alt="moi en photo" loading="lazy" />
       </div>
     </div>
 
     <div class="quete">
       <p>
-        {{ $t("accueil.quete1") }} <h4>{{ $t("accueil.quete2") }}</h4> {{ $t("accueil.quete3") }} <h4>{{ $t("accueil.quete4") }}</h4> {{ $t("accueil.quete5") }} </p>
-
+        {{ $t('accueil.quete1') }}
+      </p>
 
       <div class="monCV">
-
         <font-awesome-icon class="fa-solid" icon="fa-arrow-right" />
         <font-awesome-icon class="fa-solid" icon="fa-angles-down" />
         <font-awesome-icon class="fa-solid" icon="fa-arrow-turn-down" />
         <font-awesome-icon class="fa-solid" icon="fa-arrow-trend-down" />
         <p>
           <a
-          href="https://drive.google.com/file/d/1gurHMCeUayjvGIYSBpYY24YAiUiw6At3/view?usp=drive_link"
-          target="_blank"
-          > <span class="ICI">{{ $t("accueil.la") }}</span> {{ $t("accueil.cv") }}
-        </a>
-      </p>
+            href="https://drive.google.com/file/d/1gurHMCeUayjvGIYSBpYY24YAiUiw6At3/view?usp=drive_link"
+            target="_blank"
+          >
+            <span class="ICI">{{ $t('accueil.la') }}</span> {{ $t('accueil.cv') }}
+          </a>
+        </p>
         <font-awesome-icon class="fa-solid" icon="fa-arrow-rotate-right" />
         <font-awesome-icon class="fa-solid" icon="fa-arrow-trend-up" />
         <span class="fa-solid with-bg">➜</span>
         <font-awesome-icon class="fa-solid" icon="fa-location-arrow" />
         <font-awesome-icon class="fa-solid" icon="fa-up-long" />
-      
       </div>
     </div>
 
     <span class="trait"></span>
 
     <Technos />
-
   </div>
 </template>
