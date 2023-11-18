3fa2d811c3d4db759df80980ee59db2b3859e8ca
Baptiste Courtin <bap2910@gmail.com>
2023-11-18 14:37:00 +0100

    creation 404 + redirect

--- src/router.js
+++ src/router.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from './views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL), // ???
@@ -7,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('./views/HomeView.vue')
     },
     {
       path: '/themes',
@@ -31,12 +30,12 @@ const router = createRouter({
       path: '/blog',
       name: 'blog',
       component: () => import('./views/BlogView.vue')
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'notFound',
+      component: () => import('./views/NotFound.vue')
     }
-    // {
-    //   path: '/:catchAll(.*)',
-    //   name: 'NotFound',
-    //   component: NotFount
-    // }
   ]
 })
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+  <div class="global-404">
+    <h1>404</h1>
+  </div>
+</template>
